4ab74354ff5ee8307546e7d6e6248e0258873d43
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-01-23 09:54:15 -0600

    ciclo de 1 a 81, url= del server, metodo= GET, succes= callback que se ejecutará cuando tengamos los datos listos, dentro de succes vamos a generar el HTMl para visualizar la imagen. utilizando el selector de clase y cla clase pokemon y haremos referencia a la lista de tipo desordenada, a esta le aplicaremos el metodo opens, como parametro enviaremos una etiqueta li que contengga una imagen, y como suource pasaremos la direcion URL que nos devuelve el servidor.

---
+++ app.js
@@ -0,0 +1,10 @@
+for (var i=1;i<=80;i ++){
+  $.ajax({
+    url: 'http://pokeapi.co/api/v2/pokemon-form/' + i,
+    type:'GET',
+    data:{},
+    success: function(data){
+      $('.pokemons').append('<li><img src="'+data.sprites.front_default+'"></img></li>')
+    }
+  })
+}
